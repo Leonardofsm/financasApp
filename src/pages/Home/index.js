b864d7f44ec6d7150a2df43f1935a74e49735459
Leonardo <leonardofsm@Outlook.com>
2022-10-07 02:48:35 -0300

    funcao para logar usuario no app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
 
+import { AuthContext } from '../../contexts/auth';
+
 export default function Home() {
+
+  const { user } = useContext(AuthContext);
  return (
    <View>
        <Text>Home</Text>
+       <Text>Bem vindo: {user && user.nome }</Text>
+       <Text>Email: {user && user.email }</Text>
    </View>
   );
 }
