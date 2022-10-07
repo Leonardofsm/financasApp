333acc711eb204e71693aa4adcbd47775866e679
Leonardo <leonardofsm@Outlook.com>
2022-10-07 02:56:38 -0300

    limpando state e saindo do app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,19 @@
 import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth';
 
 export default function Home() {
 
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
  return (
    <View>
        <Text>Home</Text>
        <Text>Bem vindo: {user && user.nome }</Text>
        <Text>Email: {user && user.email }</Text>
+       <Button 
+       title="Sair da conta"
+       onPress={ () => signOut() } />
    </View>
   );
 }
